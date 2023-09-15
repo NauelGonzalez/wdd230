aea6e5e3bc92225b9a35d292354641cc3fd51285
NauelGonzalez <nrgonzi@gmail.com>
2023-09-15 11:36:37 -0300

    home site

---
+++ scripts/getDates.js
@@ -0,0 +1,15 @@
+document.getElementById("date").textContent =
+  "© " + new Date().getFullYear() + " | Nauel Gonzalez | Uruguay";
+
+const lastModified = new Date(document.lastModified);
+const format = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
+document.getElementById("lastModified").textContent =
+  "Last modification: " + lastModified.toLocaleString("en-US", format);
