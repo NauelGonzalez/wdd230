4d38c3add1a6a091e3b65db4caf2a08aa1eadbf0
NauelGonzalez <nrgonzi@gmail.com>
2023-10-10 00:33:39 -0300

    dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,26 @@
 const baseURL = "https://nauelgonzalez.github.io/wdd230/";
 const linksURL = "https://nauelgonzalez.github.io/wdd230/data/links.json";
+
+const ul = document.getElementById("links");
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data.weeks);
+}
+
+function displayLinks(weeks) {
+  weeks.forEach((week) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${week.week}: `;
+    week.links.forEach((link) => {
+      let a = document.createElement("a");
+      a.setAttribute("href", link.url);
+      a.innerText = link.title + " | ";
+      li.append(a);
+    });
+    ul.appendChild(li);
+  });
+}
+
+getLinks();
