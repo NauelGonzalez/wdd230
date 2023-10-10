866539de9efbad01abcc2730a1ba45d64444a626
NauelGonzalez <nrgonzi@gmail.com>
2023-10-09 23:41:06 -0300

    home week5

---
+++ scripts/links.js
@@ -0,0 +1,2 @@
+const baseURL = "https://nauelgonzalez.github.io/wdd230/";
+const linksURL = "https://nauelgonzalez.github.io/wdd230/data/links.json";
---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+/////////////////// weather ///////////////////////////////////////
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=-34.8883606&lon=-56.0741786&units=metric&appid=3d13a5faf31421d6bc1ef85ff2ec23bd";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  const iconalt = `${data.weather[0].main}`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", iconalt);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
