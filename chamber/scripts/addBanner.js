d2abee73c933aec690549d8207e5824b579fc2be
NauelGonzalez <nrgonzi@gmail.com>
2023-10-21 00:27:26 -0300

    add banner

--- chamber/scripts/addBanner.js
+++ chamber/scripts/addBanner.js
@@ -6,7 +6,7 @@ const bannerBtn = document.getElementById("banner-close");
 const today = new Date();
 const dayOfWeek = today.getDay();
 
-if (dayOfWeek === 1 || dayOfWeek === 2 || dayOfWeek === 3) {
+if (dayOfWeek === 1 || dayOfWeek === 2 || dayOfWeek === 6) {
   bannerDiv.classList.remove("hidden");
 }
 
